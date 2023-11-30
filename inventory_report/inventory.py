2c6eac91bc5c70e0ae982d99b31220296f54e830
Danillo Santos <dnashy@hotmail.com>
2023-11-30 16:20:04 -0300

    feat: create SimpleReport class and add more methods into the Inventory class

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,5 +1,7 @@
 from typing import Optional, List
 from inventory_report.product import Product
+from datetime import datetime
+from collections import Counter
 
 
 class Inventory:
@@ -12,3 +14,41 @@ class Inventory:
 
     def add_data(self, data: List[Product]):
         self.data.extend(data)
+
+    def get_earliest_date(self) -> str:
+        if not self.data:
+            return "No manufacturing dates available"
+
+        earliest_date = min(
+            [product.manufacturing_date for product in self.data]
+        )
+
+        return earliest_date
+
+    def get_latest_date(self) -> str:
+        current_date = datetime.today()
+
+        valid_dates = [
+            product.expiration_date
+            for product in self.data
+            if (
+                datetime.strptime(product.expiration_date, "%Y-%m-%d")
+                > current_date
+            )
+        ]
+
+        if not valid_dates:
+            return "No valid expiration dates available"
+
+        latest_date = min(valid_dates)
+        return latest_date
+
+    def get_company(self) -> str:
+        if not self.data:
+            return "No companies available"
+
+        company_with_more_products = Counter(
+            [product.company_name for product in self.data]
+        )
+
+        return company_with_more_products.most_common(1)[0][0]
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,29 @@
-class SimpleReport:
-    pass
+from inventory_report.inventory import Inventory
+from inventory_report.reports.report import Report
+
+
+class SimpleReport(Report):
+    def __init__(self):
+        self.inventories = []
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        self.inventories.append(inventory)
+
+    def generate(self) -> str:
+        oldest = None
+        expiration = None
+        company = None
+
+        for inventory in self.inventories:
+            if not oldest or oldest > inventory.get_earliest_date():
+                oldest = inventory.get_earliest_date()
+            if not expiration or expiration < inventory.get_latest_date():
+                expiration = inventory.get_latest_date()
+            if not company or company < inventory.get_company():
+                company = inventory.get_company()
+
+        return (
+            f"Oldest manufacturing date: {oldest}\n"
+            f"Closest expiration date: {expiration}\n"
+            f"Company with the largest inventory: {company}"
+        )
