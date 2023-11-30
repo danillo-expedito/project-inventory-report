debe9ce6068152cc9aae06a43f9c629ffb2d69f3
Danillo Santos <dnashy@hotmail.com>
2023-11-30 17:00:46 -0300

    feat: create the inputHandler/ cli working

--- inventory_report/cli/input_handler.py
+++ inventory_report/cli/input_handler.py
@@ -1,4 +1,8 @@
 from typing import List
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.importers import JsonImporter, CsvImporter
+from inventory_report.inventory import Inventory
 
 
 def process_report_request(file_paths: List[str], report_type: str) -> str:
@@ -6,4 +10,16 @@ def process_report_request(file_paths: List[str], report_type: str) -> str:
     Process the report given a list of file paths and a report type,
     and returns the result.
     """
-    raise NotImplementedError
+    if not report_type == "simple" and not report_type == "complete":
+        raise ValueError("Report type is invalid.")
+
+    report = CompleteReport() if report_type == "complete" else SimpleReport()
+
+    for path in file_paths:
+        file_extension = path.split(".")[-1]
+        if file_extension == "json":
+            report.add_inventory(Inventory(JsonImporter(path).import_data()))
+        elif file_extension == "csv":
+            report.add_inventory(Inventory(CsvImporter(path).import_data()))
+
+    return report.generate()
