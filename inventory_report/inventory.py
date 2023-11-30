086dadc392c709e6c1377f255172e50803c80314
Danillo Santos <dnashy@hotmail.com>
2023-11-30 15:06:52 -0300

    feat: add a new class inventory that stores all the products

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from typing import Optional, List
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None):
+        self._data = data or []
+
+    @property
+    def data(self):
+        return self._data
+
+    def add_data(self, data: List[Product]):
+        self.data.extend(data)
