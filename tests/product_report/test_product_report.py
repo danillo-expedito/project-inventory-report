1a09b7b3752c7bd501b921bc3203bb8537d199ac
Danillo Santos <dnashy@hotmail.com>
2023-11-30 14:24:44 -0300

    test: add tests to the product_report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,24 @@
-def test_product_report() -> None:
-    raise NotImplementedError
+from inventory_report.product import Product
+import pytest
+
+
+@pytest.fixture
+def test_product() -> Product:
+    return Product(
+        "1",
+        "Coca Cola",
+        "Coca Cola Company",
+        "2023-08-01",
+        "2024-12-01",
+        "123456",
+        "Keep in dry and cool place"
+    )
+
+
+def test_product_report(test_product) -> None:
+    assert str(test_product) == (
+        "The product 1 - Coca Cola with serial number 123456 "
+        "manufactured on 2023-08-01 by the company Coca Cola Company "
+        "valid until 2024-12-01 must be stored according to the following "
+        "instructions: Keep in dry and cool place."
+    )
